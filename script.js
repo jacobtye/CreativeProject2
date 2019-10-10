b08b5b7b387e9cf20fa99c56978e5dbe185ed9f5
jacobtye <jacob.h.tye@gmail.com>
2019-10-10 21:25:49 +0000

    working on css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*global Vue*/
+/*global fetch*/
 let app = new Vue({
   // bind it to the #root div in the DOM
   el: "#root",
@@ -5,20 +7,18 @@ let app = new Vue({
   data: {
     message: 'USD',
     amount: 0,
+    rate: {},
   },
-//   method: {
-//       updateData(newJson){
-//           message = newJson.rates;
-//           return message;
-//       }
-//   }
+
 });
 async function parseJson(arg){
     const response = await fetch('https://api.exchangerate-api.com/v4/latest/' + arg);
     var json = await response.json();
     // var results = await app.updateData(json);
     app.message = Object.keys(json.rates);
-    console.log(app.message);
+    app.rate = json.rates;
+    console.log(json.rates);
+    // console.log(app.message);
     // console.log(Object.keys(json.rates));
     // console.log(json);
 }
